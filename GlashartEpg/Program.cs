d3dbdbcbfc39cea5129515ec13541a07b1d6ad56
Jan Saris <jan.saris@gmail.com>
2015-05-02 15:28:32 +0200

    Added loging and wrote first line

--- GlashartEpg/Program.cs
+++ GlashartEpg/Program.cs
@@ -1,25 +1,30 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using log4net;
+using log4net.Config;
 
 namespace GlashartEpg
 {
     class Program
     {
+        private static readonly ILog Logger = LogManager.GetLogger(typeof(Program));
 
-
-        static void Main(string[] args)
+        static void Main()
         {
             try
             {
-
+                var program = new Program();
+                program.Run();
             }
             catch (Exception ex)
             {
-                
+                Logger.Fatal("An unhandled error occured", ex);
             }
+        }
+
+        private void Run()
+        {
+            Logger.Info("Weclome to Glashart EPG grabber");
+
         }
     }
 }
