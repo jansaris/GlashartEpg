35ac4176c5b4d2e11ad3b604accf09545481fde7
Jan Saris <jan.saris@gmail.com>
2015-05-02 15:11:44 +0200

    First commit

---
+++ GlashartEpg/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GlashartEpg
+{
+    class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+            try
+            {
+
+            }
+            catch (Exception ex)
+            {
+                
+            }
+        }
+    }
+}
